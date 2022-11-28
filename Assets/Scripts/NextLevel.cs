04cf2ecf32df3f707a897a5c386df13f3c947f6d
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2022-11-28 17:35:13 -0500

    gjrfburh

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -8,22 +8,36 @@ public class NextLevel : MonoBehaviour
     public GameObject finalPlatform;
     public float skinWidth;
     public bool Win;
-
+    public bool spawnerPresent;
+    public GameObject[] spawners;
+    public Material red;
+    public Material yellow;
+    private void Start()
+    {
+        spawnerPresent = true;
+        spawners = GameObject.FindGameObjectsWithTag("Spawner");
+    }
     public void Update()
     {
+       
        RaycastHit hit;
        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit))
         {
-            if (hit.collider.gameObject.name == "final" && SceneManager.GetActiveScene().name != "Level_2")
+            if (hit.collider.gameObject.name == "final" && SceneManager.GetActiveScene().name != "Level_2" && spawners[0] == null) 
             {
                 SceneManager.LoadScene("win");
             }
-            else if(hit.collider.gameObject.name == "final" && SceneManager.GetActiveScene().name == "Level_2")
+            else if(hit.collider.gameObject.name == "final" && SceneManager.GetActiveScene().name == "Level_2" && spawners[0] == null)
             {
                 SceneManager.LoadScene("GameEnd");
             }
+            else if (hit.collider.gameObject.name == "final" && spawners[0] != null)
+            {
+                hit.collider.gameObject.GetComponent<MeshRenderer>().material = red;
+            }
             else
             {
+                hit.collider.gameObject.GetComponent<MeshRenderer>().material = yellow;
                 return;
             }
         }
