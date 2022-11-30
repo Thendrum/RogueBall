e0c3daaa4e0c4625f5296d298fb8f0f112d056ed
Thendrum <77357730+Thendrum@users.noreply.github.com>
2022-11-30 18:04:51 -0500

    Lava Tutorial
    Lava tutorial script and UI implemented
    Tutorial level excess space trimmed

--- Assets/Scripts/TutorialUI.cs
+++ Assets/Scripts/TutorialUI.cs
@@ -6,7 +6,8 @@ using JetBrains.Annotations;
 
 public class TutorialUI : MonoBehaviour
 {
-    
+    public GameObject LavaUI;
+    public float skinWidth;
     public GameObject JumpTutorial;
     void Update()
     {
@@ -15,5 +16,21 @@ public class TutorialUI : MonoBehaviour
         {
             JumpTutorial.SetActive(false);
         }
+        
+        //Probably better to cast the ray from the player but this is the way I did it
+        //Ray casts upwards from the object 
+        RaycastHit hit;
+        Ray Platform = new Ray(transform.position, Vector3.up);
+        if (Physics.Raycast(Platform, out hit, skinWidth))
+        {
+            LavaUI.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            LavaUI.SetActive(false);
+        }
     }
+
+   
 }
