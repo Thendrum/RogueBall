c3240df146d2c8466720a7781205bfb7428c5ead
TannerEubanks <61129318+TannerEubanks@users.noreply.github.com>
2022-12-04 14:26:08 -0500

    post processing and music fix

--- Assets/Scripts/BackgroudMusic.cs
+++ Assets/Scripts/BackgroudMusic.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BackgroudMusic : MonoBehaviour
 {
+    public AudioSource source;
+    public int numberOfClassObjects;
     // Start is called before the first frame update
     void Start()
     {
+        numberOfClassObjects = GameObject.FindGameObjectsWithTag("music").Length;
+        Debug.Log(numberOfClassObjects);
+        source = this.GetComponent<AudioSource>();
+        if(numberOfClassObjects <= 1)
+        {
+            source.Play();
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        numberOfClassObjects = GameObject.FindGameObjectsWithTag("music").Length;
         DontDestroyOnLoad(this);
     }
 }
